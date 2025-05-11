8c466eb1c589e3647abcd3fba98c8dd5b37a09c5
brjatoba92 <brjatoba92@gmail.com>
2025-05-11 17:25:44 -0300

    Navbar droping using tailwind

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import Logo from "../assets/website/logo.png";
+import { FaCaretDown} from "react-icons/fa";
+import { FaCartShopping } from "react-icons/fa6";
 
 const Menu = [
     {
@@ -13,31 +15,36 @@ const Menu = [
         name: "Best Seller",
         link: "/#",
     },
+    
+]
+
+const DropdownLinks = [
     {
-        id: 3,
-        name: "About",
+        name: "Trending Books",
         link: "/#",
     },
     {
-        id: 4,
-        name: "Contact",
+        name: "Best Selling",
+        link: "/#",
+    },
+    {
+        name: "Authors",
         link: "/#",
     }
 ]
 
 const Navbar = () => {
     return (
-        <div>
+        <div className="shadow-lg">
             <div className="container py-3 sm:py-0">
                 <div className="flex justify-between items-center">
                     <div>
-                        <a href={"#"} className="font-bold text-2xl sm:text-3xl flex gap-2">
+                        <a href={"/#"} className="font-bold text-2xl sm:text-3xl flex gap-2">
                             <img src={Logo} alt="" className="w-10"/>
                             Books
                         </a>
-                        
                     </div>
-                    <div>
+                    <div className="flex items-center justify-beetwen gap-4 ">
                         <ul className="items-center gap-4 hidden sm:flex">
                            {Menu.map((menu) => (
                             <li key={menu.id}>
@@ -49,7 +56,33 @@ const Navbar = () => {
                                 </a>
                             </li>
                            ))}
+                           {/* dropdown section */}
+                           <li className="group relative cursor-pointer">
+                            <a href="/#" className="flex h-[72px] items-center gap[2px]">
+                                Quick Links
+                                <span>
+                                    <FaCaretDown className="transition duration-200 group-hover:rotate-180" />
+                                </span>
+                            </a>
+                            {/* dropdown link section */}
+                            <div className="absolute -left-9 z-[10] hidden group-hover:block text-black bg-white p-2 shadow-md w-[150px]">
+                                <ul>
+                                    {DropdownLinks.map((data) => (
+                                        <li key={data.id}>
+                                            <a href={data.link} className="inline-block w-full rounded-md p-2 hover:bg-primary/20 shadow-md">
+                                                {data.name}
+                                            </a>
+                                        </li>
+                                    ))}
+                                </ul>
+                                
+                            </div>
+                            </li>
                         </ul>
+                        <button className="bg-gradient-to-r from-primary to-secondary text-white px-4 py-2 rounded-full flex items-center gap-3 hover:scale-105 duration-300">
+                            Order
+                            <FaCartShopping className="text-xl text-white drop-shadow-sm cursor-pointer" />
+                        </button>
                     </div>
                 </div>
             </div>
