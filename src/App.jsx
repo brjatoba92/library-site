6f26b257097cbc6230db43583a30967dab434855
brjatoba92 <brjatoba92@gmail.com>
2025-05-17 00:23:22 -0300

    Best Books Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero/Hero";
+import BestBooks from "./components/BestBooks/BestBooks";
 
 const App = () => {
   return <>
-    <div>
+    <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
       <Navbar />
       <Hero />
+      <BestBooks />
     </div>
   </>;
 };
