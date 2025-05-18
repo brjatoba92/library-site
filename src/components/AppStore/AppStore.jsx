3dd3f991e49ce085e25de66d32779723520012d2
brjatoba92 <brjatoba92@gmail.com>
2025-05-17 23:20:41 -0300

    App Store Banner Section Created

---
+++ src/components/AppStore/AppStore.jsx
@@ -0,0 +1,22 @@
+import PlayStoreImg from "../../assets/website/playstore.png";
+import AppStoreImg from "../../assets/website/appstore.png";
+import Banner from "../../assets/website/banner.jpg";
+
+
+const bannerImg = {
+    backgroundImage: `url(${Banner})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    backgroundRepeat: "no-repeat",
+    width: "100%",
+    height: "100%",
+}
+const AppStore = () => {
+    return (
+        <>
+            <div>
+
+            </div>
+        </>
+    )
+}
