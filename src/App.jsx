dd7d550b9af7e99783a1de087fe9f397d98cb6fa
brjatoba92 <brjatoba92@gmail.com>
2025-05-21 15:39:20 -0300

    Order Popup Created

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,19 @@ import AppStoreBanner from "./components/AppStoreBanner/AppStoreBanner";
 import AllBooks from "./components/AllBooks/AllBooks";
 import Testemonial from "./components/Testemonial/Testemonial";
 import Footer from "./components/Footer/Footer";
+import Popup from "./components/Popup/Popup";
 
 //AOS import
 import AOS from "aos";
 import "aos/dist/aos.css";
 
 const App = () => {
+
+  const [orderPopup, setOrderPopup] = React.useState(false);
+
+  const handleOrderPopup = () => {
+    setOrderPopup(!orderPopup);
+  };
 
   useEffect(() => {
     AOS.init(
@@ -28,14 +35,15 @@ const App = () => {
 
   return <>
     <div className="bg-white dark:bg-gray-900 dark:text-white duration-200">
-      <Navbar />
-      <Hero />
-      <BestBooks />
+      <Navbar handleOrderPopup={handleOrderPopup}/>
+      <Hero handleOrderPopup={handleOrderPopup}/>
+      <BestBooks handleOrderPopup={handleOrderPopup}/>
       <Banner />
       <AppStoreBanner />
       <AllBooks />
       <Testemonial />
       <Footer />
+      <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
     </div>
   </>;
 };
